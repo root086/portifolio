383ed8c12aa574c84e7ea27b3b94a5dfc9aba90b
lucasvittor <dev.lucssvitor@gmail.com>
2025-08-06 13:16:09 -0300

    Update - New section (about me)

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-    lucide.createIcons();
-    function toggleMenu() {
-      const sidebar = document.querySelector('.sidebar');
-      const overlay = document.querySelector('.overlay');
-      sidebar.classList.toggle('open');
-      overlay.classList.toggle('show');
+  lucide.createIcons();
+
+  function toggleMenu() {
+    const sidebar = document.querySelector('.sidebar');
+    const overlay = document.querySelector('.overlay');
+    const menuIcon = document.getElementById('menuIcon');
+
+    sidebar.classList.toggle('open');
+    overlay.classList.toggle('show');
+
+    if (sidebar.classList.contains('open')) {
+      menuIcon.setAttribute("data-lucide", "x");
+    } else {
+      menuIcon.setAttribute("data-lucide", "menu");
     }
+  }
+
+    function scrollToProjects(event) {
+    event.preventDefault(); 
+    const section = document.getElementById("project");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  }
